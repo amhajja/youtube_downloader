ae24348b128dab8c417a7ccfc2049ece38ee866d
Ayman H <amhajja@gmail.com>
2022-05-30 08:02:46 +0200

    Start creating a 'yt_common' library

---
+++ yt_common/download.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+
+def download_video(link):
+    try:
+        yt = YouTube(link)
+    except:
+        print("Connection Error")  # to handle exception
+
+    try:
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-2]
+        stream.download('downloaded_videos')
+    except:
+        print("Some Error!")
+    print('Task Completed!')
