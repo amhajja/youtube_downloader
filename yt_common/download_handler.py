108b1b29aee5dc99a1b7897a7bb88715270464e0
Ayman H <amhajja@gmail.com>
2022-07-05 19:40:15 +0300

    Add all files

--- yt_common/download_handler.py
+++ yt_common/download_handler.py
@@ -5,11 +5,15 @@ def download_video(link):
     try:
         yt = YouTube(link)
     except:
-        print("Connection Error")  # to handle exception
+        print("Connection Error")
+        return False
 
     try:
         stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-2]
         stream.download('downloaded_videos')
     except:
         print("Some Error!")
+        return False
+
     print('Task Completed!')
+    return True
