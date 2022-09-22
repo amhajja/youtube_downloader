234a3327c2c869a065ea4127750376d9aee74307
Ayman H <amhajja@gmail.com>
2022-09-21 20:47:24 -0500

    Add a output_dir parameter to specify location of saved file

--- yt_common/download_handler.py
+++ yt_common/download_handler.py
@@ -1,17 +1,18 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 
 
-def download_video(link):
+def download_video(link: str, output_dir: str = 'downloaded_videos'):
     try:
-        yt = YouTube(link)
+        yt = YouTube(link, on_progress_callback=on_progress)
     except:
         print("Connection Error")
         return False
 
     try:
         print("Started downloading video")
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-2]
-        stream.download('downloaded_videos')
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1]
+        stream.download(output_dir)
     except:
         print("Some Error!")
         return False
